e872047b183b704231ca5a3c82b96ca3cfdd4636
totenhund <caligoyan@gmail.com>
2020-09-27 02:57:52 +0600

    add comments

--- main.py
+++ main.py
@@ -3,23 +3,27 @@ from datetime import datetime
 from time import sleep
 
 
+# prints the local Lamport timestamp
 def local_time(counter):
     return ' (LAMPORT_TIME={}, LOCAL_TIME={})'.format(counter,
                                                       datetime.now())
 
 
+# calculates the new timestamp when a process receives a message
 def calc_recv_timestamp(recv_time_stamp, counter):
     for id in range(len(counter)):
         counter[id] = max(recv_time_stamp[id], counter[id])
     return counter
 
 
+# show the event took place and returns the incremented array counter.
 def event(pid, counter):
     counter[pid] += 1
     print('Something happened in {} !'.format(pid) + local_time(counter))
     return counter
 
 
+# To send or receive a message we need to call the send or recv function on these connection object.
 def send_message(pipe, pid, counter):
     counter[pid] += 1
     pipe.send(('Empty shell', counter))
@@ -33,7 +37,7 @@ def recv_message(pipe, pid, counter):
     print('Message received at ' + str(pid) + local_time(counter))
     return counter
 
-
+# process a
 def process_one(pipe12):
     pid = 0
     counter = [0, 0, 0]
@@ -44,10 +48,11 @@ def process_one(pipe12):
     counter = event(pid, counter)
     counter = event(pid, counter)
     counter = recv_message(pipe12, pid, counter)
-    sleep(1)
+    sleep(1)  # sort output
     print('Process a {}'.format(counter))
 
 
+# process b
 def process_two(pipe21, pipe23):
     pid = 1
     counter = [0, 0, 0]
@@ -63,6 +68,7 @@ def process_two(pipe21, pipe23):
     print('Process b {}'.format(counter))
 
 
+# process c
 def process_three(pipe32):
     pid = 2
     counter = [0, 0, 0]
@@ -78,6 +84,7 @@ if __name__ == '__main__':
     oneandtwo, twoandone = Pipe()
     twoandthree, threeandtwo = Pipe()
 
+    # init processes
     process1 = Process(target=process_one,
                        args=(oneandtwo,))
     process2 = Process(target=process_two,
@@ -85,6 +92,7 @@ if __name__ == '__main__':
     process3 = Process(target=process_three,
                        args=(threeandtwo,))
 
+    # start processes
     process1.start()
     process2.start()
     process3.start()
